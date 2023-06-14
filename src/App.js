2efbb73127bba84187ff2984203a62c28ed95ab3
edyta <edytakr12@gmail.com>
2023-06-14 18:27:12 +0200

    dodano strony

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import DetailsPage from "./pages/DetailsPage/DetailsPage";
+import SearchPage from "./pages/SearchPage/SearchPage";
+import ActorPage from "./pages/ActorPage/ActorPage";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/details/:id" element={<DetailsPage />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/actor/:name" element={<ActorPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
