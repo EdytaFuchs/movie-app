51254a219672c2ae8c1b63b64fd141d37cf4122d
edyta <edytakr12@gmail.com>
2023-06-16 20:12:10 +0200

    karta jerdna pod drugą

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -2,15 +2,15 @@ import React from "react";
 import styles from "./MovieCard.module.css";
 
 const MovieCard = ({ movie }) => {
-  const { title, poster_path, overview } = movie;
-  const imageUrl = "https://image.tmdb.org/t/p/w500/${poster_path}";
+  const { title, poster_path } = movie;
+  const imageUrl = `https://image.tmdb.org/t/p/w500/${poster_path}`;
+  console.log(poster_path);
 
   return (
     <div className={styles.card}>
       <img className={styles.poster} src={imageUrl} alt={title} />
       <div className={styles.content}>
         <h3 className={styles.title}>{title}</h3>
-        <p className={styles.overview}>{overview}</p>
       </div>
     </div>
   );
