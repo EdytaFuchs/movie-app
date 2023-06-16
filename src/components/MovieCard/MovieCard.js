282a4ed9a6f1f159dd5f88a74692e2e66ec5cdf2
edyta <edytakr12@gmail.com>
2023-06-16 19:49:16 +0200

    dodano pierwsze stylowanie

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,10 +1,17 @@
 import React from "react";
+import styles from "./MovieCard.module.css";
 
 const MovieCard = ({ movie }) => {
-  console.log(movie);
+  const { title, poster_path, overview } = movie;
+  const imageUrl = "https://image.tmdb.org/t/p/w500/${poster_path}";
+
   return (
-    <div>
-      <h3>{movie.title}</h3>
+    <div className={styles.card}>
+      <img className={styles.poster} src={imageUrl} alt={title} />
+      <div className={styles.content}>
+        <h3 className={styles.title}>{title}</h3>
+        <p className={styles.overview}>{overview}</p>
+      </div>
     </div>
   );
 };
