d6b467bd9586b393a6bc451bb33ad65b34fa183d
edyta <edytakr12@gmail.com>
2023-06-19 20:01:50 +0200

    dodano informacje do filmów

--- src/pages/DetailsPage/DetailsPage.js
+++ src/pages/DetailsPage/DetailsPage.js
@@ -1,17 +1,32 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { useFetch } from "../../hooks/useFetch";
+import styles from "./DetailsPage.module.css";
 
 const DetailsPage = () => {
   const params = useParams();
   const movie = useFetch(`/movie/${params.id}`);
 
+  const { original_title, poster_path, overview, release_date } = movie.data;
   console.log(movie.data);
   console.log(params);
 
+  const imageUrl = `https://image.tmdb.org/t/p/w500/${poster_path}`;
+
   return (
-    <div>
-      <h1>{movie.data.original_title}</h1>
+    <div className={styles.container}>
+      <h1 className={styles.title}>{original_title}</h1>
+      <div>
+        <img className={styles.image} src={imageUrl} alt={original_title} />
+      </div>
+      <div className={styles.overview}>
+        <h2>Overview</h2>
+        <p>{overview}</p>
+      </div>
+      <div className={styles.release_date}>
+        <h2>Release Date</h2>
+        <p>{release_date}</p>
+      </div>
     </div>
   );
 };
